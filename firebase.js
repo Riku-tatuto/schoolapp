33041c7a1cb4ff34f89f1aff48a0c26c4e400a22
Riku-tatuto <Riku_tatuto@outlook.com>
2025-04-14 21:11:25 +0900

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,9 +1,8 @@
 // firebase.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-app.js";
-import { getAuth, onAuthStateChanged, GoogleAuthProvider } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-auth.js";
+import { getAuth, GoogleAuthProvider } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-firestore.js";
 
-// 🔧 Firebase設定
 const firebaseConfig = {
   apiKey: "AIzaSyD7ouobvvAMBOg76rIdbFZiHEHzZH0q4FA",
   authDomain: "schoolapp-db.firebaseapp.com",
@@ -13,10 +12,7 @@ const firebaseConfig = {
   appId: "1:28108935218:web:f60e76dbc3c0cca9586c2b"
 };
 
-// 🔄 初期化
 const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-const db = getFirestore(app);
-const provider = new GoogleAuthProvider();
-
-export { auth, db, onAuthStateChanged, provider };
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+export const provider = new GoogleAuthProvider();
